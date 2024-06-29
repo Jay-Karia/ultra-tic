f0ad7a9d4e5003d6017f3cc6183a5ed03e62e86d
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-06-29 11:53:39 +0530

    improved home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/ui/button";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
+import Link from "next/link";
+
 import localFont from "next/font/local"
 import { Poppins } from 'next/font/google'
 import { cn } from "@/lib/utils";
@@ -13,14 +18,27 @@ const textFont = Poppins({
 
 export default function Home() {
   return (
-    <div className="w-full h-full flex justify-center my-10">
-      <div className="flex flex-col items-center space-y-2">
+    <div className="w-full h-full flex flex-col items-center py-10 space-y-12">
+      <div className="flex flex-col items-center space-y-5 w-full">
+
         <h2 className={cn("scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center", headingFont.className)}>
           <span className="text-red-600">Ultra</span> Tic Tac Toe
         </h2>
-        <p className="text-sm text-muted-foreground text-center max-w-80">
-        Achieve a three-in-a-row victory across multiple nested 3x3 grids within a larger board.
+        <p className={cn("text-sm text-muted-foreground text-center max-w-80", textFont.className)}>
+          Achieve a three-in-a-row victory across multiple nested 3x3 grids within a larger board.
         </p>
+
+        <Button variant={"outline"} asChild>
+          <Link href="https://github.com/Jay-Karia/ultra-tic" target="_blank">
+            <GitHubLogoIcon className="w-4 h-4 mr-2" />
+            GitHub
+          </Link>
+        </Button>
+      </div>
+
+      <div className="flex justify-center w-full">
+        {/* Game Board */}
+        Game board
       </div>
     </div>
   );
