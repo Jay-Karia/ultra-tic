39e4519fe35c07cfff1d722f07bb28fac86e453e
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-06-30 17:01:54 +0530

    fix: outer board draw bug

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -9,15 +9,15 @@ import checkWin from "@/lib/checkWin";
 
 export default function Game() {
 
-    const [board, setBoard] = useState<BoardValue[]>([null, null, null, null, null, null, null, null, null]);
+    const [board, setBoard] = useState<BoardValue[]>([1, 2, 2, 1, 1, 2, 2, 1, 2]);
     const [turn, setTurn] = useState<Turn>(2)
     const [winner, setWinner] = useState<BoardValue>(null)
     const [reset, setReset] = useState<boolean>(false)
 
     function checkDraw(board: BoardValue[]) {
         if (board.every((box) => box !== null)) {
-            setBoard([null, null, null, null, null, null, null, null, null])
             setReset(true)
+            setBoard([null, null, null, null, null, null, null, null, null])
         }
     }
 
@@ -25,24 +25,23 @@ export default function Game() {
         const winner = checkWin(board)
         setWinner(winner)
         checkDraw(board)
-        setReset(false)
     })
 
 
     return (
 
-        <div className="flex flex-col mx-4 sm:mt-6 mt-20">
+        <div className="flex flex-col mx-4 sm:mt-4 mt-20">
 
-            <Actions turn={turn} setBoard={setBoard} setReset={setReset} setWinner={setWinner}/>
+            <Actions turn={turn} setBoard={setBoard} setReset={setReset} setWinner={setWinner} />
 
             <div className="grid grid-cols-3 grid-rows-3 border border-black">
                 {board.map((row, boardIndex) => (
-                    <Board key={boardIndex} value={board[boardIndex]} turn={turn} boardIndex={boardIndex} setTurn={setTurn} setWinner={setBoard} outerBoard={board} disable={winner} reset={reset} setReset={setReset}/>
+                    <Board key={boardIndex} value={board[boardIndex]} turn={turn} boardIndex={boardIndex} setTurn={setTurn} setWinner={setBoard} outerBoard={board} disable={winner} reset={reset} setReset={setReset} />
                 ))}
             </div>
 
             {/* Winner modal */}
-            {winner && <WinnerDialog winner={winner}/>}
+            {winner && <WinnerDialog winner={winner} />}
         </div>
     )
 }
